2b689bba4c43cd756ef2385a4f352c7a7801376d
jann_ <lazopizarro2121@gmail.com>
2022-09-28 19:47:51 -0300

    app todo recetas

--- app/src/main/java/com/example/todorecetas/MainActivity.java
+++ app/src/main/java/com/example/todorecetas/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.todorecetas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button boton;
+    EditText usernameEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton = findViewById(R.id.button1);
+        usernameEdit = findViewById(R.id.usernameEdit);
+
+        //listener para el boton, despues de presionarlo las siguientes operaciones ocurriran
+        boton.setOnClickListener(v ->{
+            //obtener valor del input y transformarlo a string
+            String str = usernameEdit.getText().toString();
+            //Crear el Intent
+            Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+            //
+            intent.putExtra("mensaje",str);
+            //iniciar el intent
+            startActivity(intent);
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/todorecetas/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.todorecetas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView);
+        // get intent
+        Intent intent = getIntent();
+        //recibir el valor mediante getStringExtra(), la llave debe ser la misma
+        String str = intent.getStringExtra("mensaje");
+        //mostrar en textView
+        textView.setText("Bienvenido: "+str);
+    }
+}
